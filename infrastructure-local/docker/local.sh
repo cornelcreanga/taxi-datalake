907a830d7e68b4180221df3434a88900b1bd6cf4
creanga <creanga@adobe.com>
2021-10-30 14:03:18 +0300

    change dependencies check for java > 1.11

--- infrastructure-local/docker/local.sh
+++ infrastructure-local/docker/local.sh
@@ -1,8 +1,46 @@
+jdk_version() {
+  local result
+  local java_cmd
+  if [[ -n $(type -p java) ]]
+  then
+    java_cmd=java
+  elif [[ (-n "$JAVA_HOME") && (-x "$JAVA_HOME/bin/java") ]]
+  then
+    java_cmd="$JAVA_HOME/bin/java"
+  fi
+  local IFS=$'\n'
+  # remove \r for Cygwin
+  local lines=$("$java_cmd" -Xms32M -Xmx32M -version 2>&1 | tr '\r' '\n')
+  if [[ -z $java_cmd ]]
+  then
+    result=0
+  else
+    for line in $lines; do
+      if [[ (-z $result) && ($line = *"version \""*) ]]
+      then
+        local ver=$(echo $line | sed -e 's/.*version "\(.*\)"\(.*\)/\1/; 1q')
+        # on macOS, sed doesn't support '?'
+        if [[ $ver = "1."* ]]
+        then
+          result=$(echo $ver | sed -e 's/1\.\([0-9]*\)\(.*\)/\1/; 1q')
+        else
+          result=$(echo $ver | sed -e 's/\([0-9]*\)\(.*\)/\1/; 1q')
+        fi
+      fi
+    done
+  fi
+  echo "$result"
+}
+
+v="$(jdk_version)"
+if [ "$v" -lt "11" ]; then
+    echo "Java version should be > 11"
+    exit
+fi
+
 cd ../../
 mvn clean package
 cd ingestion-streaming
-docker build -t filesapi .
-cd ../files-worker
 docker build -t streaming .
 
 cd ../infrastructure-local/docker
